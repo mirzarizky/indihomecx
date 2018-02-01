820a5f6e94d12aa46846eab6ff4b01394ec99443
Mirza Rizky Maulana <mirzarizky@gmail.com>
2018-02-01 12:06:10 +0700

    Model : User, Role. Redirect by role. Login,logout by scaffolding.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -25,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    //protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
@@ -35,5 +36,15 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    protected function authenticated(Request $request, $user)
+    {
+        if($user->role->name == 'admin') {
+            return redirect()->route('admin');
+        }
+        elseif ($user->role->name == 'supervisor') {
+            return redirect()->route('spv');
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,29 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hasone', function () {
+    $user = \App\User::first();
+    return $user->role->name;
+});
+
+Route::get('/hasmany', function () {
+    $role = \App\Role::first();
+    return $role->user[0]->name;
+});
+
+Route::get('/isadmin', function () {
+    $user = \App\User::first();
+    return $user->isAdmin;
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/spv', function () {
+    return 'halo spv';
+})->name('spv');
+Route::get('/admin', function () {
+    return 'halo admin';
+})->name('admin');
+
