cd0aaa2258e62358536d143bf9b01f46629828fc
Mirza Rizky Maulana <mirzarizky@gmail.com>
2018-04-15 20:13:24 +0700

    index admin

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -11,9 +11,61 @@
                         {{ session('status') }}
                     </div>
                 @endif
-
-                You are logged in!
             </div>
+
+
+                <div class="row center-block">
+                    <div class="col-md-4">
+                        <div class="panel panel-info">
+                            <div class="panel-heading text-center">User</div>
+                            <div class="panel-body">
+                                <p>Jumlah user yang terdaftar : {{ $count['users'] }}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="panel panel-info">
+                            <div class="panel-heading text-center">Survei</div>
+                            <div class="panel-body">
+                                <p>Survei terkirim : belum</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="panel panel-info">
+                            <div class="panel-heading text-center">Survei</div>
+                            <div class="panel-body">
+                                <p>{{ $count['survei'] }} Survei yang sudah berhasil diisi oleh pelanggan.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row center-block">
+                    <div class="col-md-4">
+                        <div class="panel panel-info">
+                            <div class="panel-heading text-center">Work Order</div>
+                            <div class="panel-body">
+                                <p>Jumlah work order yang keluar minggu ini : {{ $count['orders'] }} </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="panel panel-info">
+                            <div class="panel-heading text-center">STO</div>
+                            <div class="panel-body">
+                                <p>{{ $count['cabang'] }} STO yang terintergrasi dengan Indihome-CX saat ini.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="panel panel-info">
+                            <div class="panel-heading text-center">Berkas</div>
+                            <div class="panel-body">
+                                <p>Jumlah berkas yang sudah diupload ke database : {{ $count['berkas'] }} </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
         </div>
     </div>
 @endsection
