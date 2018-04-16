4704beabd80d9e3504b5bc714e34b4980ea924b5
Dyah Ayu Anindita <32387266+dyahanndta@users.noreply.github.com>
2018-04-16 23:07:06 +0700

    List user syip (#20)

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -1,47 +1,135 @@
 @extends('layouts.app')
-
-@section('content')
-    <div class="col-lg-9">
-        <div class="panel panel-default">
-            <div class="panel-heading">List User</div>
-
-            <div class="panel-body">
-                @if (session('status'))
-                    <div class="alert alert-success">
-                        {{ session('status') }}
-                    </div>
-                @endif
-                <div>
-                    <a href="{{route('admin.model.form', ['model' => 'user'])}}" class="btn btn-sm btn-primary">Tambah User</a>
-                </div>
-                <div class="table-responsive">
-                    <table class="table table-striped table-hover">
-                        <thead>
-                            <tr>
-                                <th>NIK</th>
-                                <th>Nama</th>
-                                <th>Email</th>
-                                <th>Role</th>
-                                <th>Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($users as $user)
-                            <tr>
-                                <td>{{$user->nik}}</td>
-                                <td>{{$user->name}}</td>
-                                <td>{{$user->email}}</td>
-                                <td>{{$user->role->name}}</td>
-                                <td>
-                                    <a href="{{route('admin.model.updateForm', ['model' => 'user', 'id' => $user->id])}}" class="btn btn-xs btn-info">Sunting</a>
-                                    <a href="{{route('admin.model.delete', ['model' => 'user', 'id' => $user->id])}}" class="btn btn-xs btn-danger">Hapus</a>
-                                </td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
+@section('title', 'Berkas Tersimpan')
+@section('style')
+<link href="{{asset('css/datatables/dataTables.bootstrap.min.css')}}" rel="stylesheet">
+<link href="{{asset('css/datatables/responsive.bootstrap.min.css')}}" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="{{asset('css/sweetalert/sweetalert.min.css')}}">
 @endsection
+@section('content')
+<div class="right_col" role="main">
+  <div class="">
+    <div class="row">
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel">
+          <div class="row x_title">
+            <div class="col-md-6">
+              <h3>Daftar <i>User</i></h3>
+            </div>
+            <div class="col-md-6">
+              @if (session('status'))
+              <div class="alert alert-info alert-dismissible fade in">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                <strong>Success.</strong> {{ session('status') }}
+              </div>
+              @endif
+              <div>
+                <a href="{{route('admin.model.form', ['model' => 'user'])}}" class="btn btn-default ftco-animate pull-right fa fa-plus-square" style="padding: 14px 20px; font-size: 15px;"> &nbsp &nbsp Tambah User </a>
+              </div>
+            </div>
+          </div>
+            <div class="container">
+              <div class="x_content">
+                <table id="datatable-responsive" class="table table-striped table-bordered" cellspacing="0" width="100%" alignright>
+                  <thead>
+                    <tr>
+                      <th>NIK</th>
+                      <th>Nama</th>
+                      <th>Email</th>
+                      <th>Hak Akses</th>
+                      <th>Tindakan</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @foreach($users as $user)
+                    <tr>
+                      <td>{{$user->nik}}</td>
+                      <td>{{$user->name}}</td>
+                      <td>{{$user->email}}</td>
+                      <td>{{$user->role->name}}</td>
+                      <td>
+                        <center>
+                          <a href="{{route('admin.model.updateForm', ['model' => 'user', 'id' => $user->id])}}" class="btn btn-dark ftco-animate">Ubah</a>
+                          <a onclick="hapusFunction()" class="btn btn-danger ftco-animate">Hapus</a>
+                        </center>
+                      </td>
+                    </tr>
+                    @endforeach
+                  </tbody>
+                </table>
+                <form id="hapus" method="get" style="display:none;" action="{{route('admin.model.delete', ['model' => 'user', 'id' => $user->id])}}"></form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <br/>
+@endsection
+@section('script')
+<script src="{{ asset('js/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('js/datatables/dataTables.bootstrap.min.js') }}"></script>
+<script src="{{asset('js/sweetalert/sweetalert.min.js')}}"></script>
+<script type="text/javascript">
+    //Table
+    function init_DataTables() {
+
+      console.log('run_datatables');
+
+      if (typeof($.fn.DataTable) === 'undefined') {
+        return;
+      }
+      console.log('init_DataTables');
+
+      $('#datatable').dataTable();
+
+      $('#datatable-keytable').DataTable({
+        keys: true
+      });
+
+      $('#datatable-responsive').DataTable();
+
+      $('#datatable-scroller').DataTable({
+        ajax: "js/datatables/json/scroller-demo.json",
+        deferRender: true,
+        scrollY: 380,
+        scrollCollapse: true,
+        scroller: true
+      });
+
+      $('#datatable-fixed-header').DataTable({
+        fixedHeader: true
+      });
+
+    };
+
+    // swal Hapus
+
+    function hapusFunction() {
+      event.preventDefault(); // prevent form submit
+      swal({
+          title: "Hapus User?",
+          text: "",
+          type: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#DD6B55",
+          confirmButtonText: "Hapus",
+          cancelButtonText: "Batal",
+          closeOnConfirm: false,
+          closeOnCancel: true
+        },
+        function(isConfirm) {
+          if (isConfirm) {
+            // swal("Terhapus!", "User berhasil dihapus.", "success");
+            document.getElementById("hapus").submit();
+          } else {
+            // swal("Batal", "User tidak terhapus.", "");
+          }
+        });
+    }
+
+    $(document).ready(function() {
+      init_DataTables()
+    });
+  </script>
+  @endsection
