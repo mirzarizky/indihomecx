7ed6cd6855d74d8ba6d1b95e931a848f5a32c6d0
Dyah Ayu Anindita <32387266+dyahanndta@users.noreply.github.com>
2018-04-19 22:08:20 +0700

    cabang done (#24)

--- resources/views/admin/cabang/form.blade.php
+++ resources/views/admin/cabang/form.blade.php
@@ -1,58 +1,100 @@
 @extends('layouts.app')
-
+@section('title', 'Tambah STO')
 @section('content')
-    <div class="col-lg-9">
-        <div class="panel panel-default">
-            <div class="panel-heading">Tambah Cabang</div>
-
-            <div class="panel-body">
-                @if (session('status'))
-                    <div class="alert alert-success">
-                        {{ session('status') }}
+<div class="right_col" role="main">
+  <div class="">
+    <div class="row">
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel">
+          <div class="x_title">
+            <h3>Tambah STO</h3>
+            <div class="clearfix"></div>
+          </div>
+          <center>
+            <div class="x_content">
+              <div class="container">
+                <div class="row">
+                  <form id="form-valid" data-parsley-validate class="form-horizontal form-label-left" method="POST" action="{{ route('admin.model.create', ['model' => 'sto']) }}">
+                    {{ csrf_field() }}
+                    <div class="form-group{{ $errors->has('kode') ? ' has-error' : '' }}">
+                      <label class="col-lg-3 control-label">Kode STO: *</label>
+                      <div class="col-lg-6">
+                        <input id="kode" name="kode" class="form-control" type="text" value="{{ old('kode') }}" required data-parsley-error-message="Kode STO harus diisi.">
+                        @if ($errors->has('kode'))
+                              <span class="help-block">
+                                  <strong>{{ $errors->first('kode') }}</strong>
+                              </span>
+                        @endif
+                      </div>
                     </div>
-                @endif
-
-                    <form class="form-horizontal" method="POST" action="{{ route('admin.model.create', ['model' => 'sto']) }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('kode') ? ' has-error' : '' }}">
-                            <label for="kode" class="col-md-4 control-label">Kode STO</label>
-
-                            <div class="col-md-6">
-                                <input id="kode" type="text" class="form-control" name="kode" value="{{ old('kode') }}" required autofocus>
-
-                                @if ($errors->has('kode'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('kode') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('nama') ? ' has-error' : '' }}">
-                            <label for="nama" class="col-md-4 control-label">Nama STO</label>
-
-                            <div class="col-md-6">
-                                <input id="nama" type="text" class="form-control" name="nama" value="{{ old('nama') }}" required>
-
-                                @if ($errors->has('nama'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('nama') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Tambah
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    <div class="form-group{{ $errors->has('nama') ? ' has-error' : '' }}">
+                      <label class="col-lg-3 control-label">Nama STO: *</label>
+                      <div class="col-lg-6">
+                        <input id="nama" name="nama" class="form-control" type="text" value="{{ old('nama') }}" required data-parsley-error-message="Nama STO harus diisi.">
+                        @if ($errors->has('nama'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('nama') }}</strong>
+                            </span>
+                        @endif
+                      </div>
+                    </div>
+                    </div>
+                    <div class="form-group">
+                      <label class="col-md-3 control-label"></label>
+                      <div class="col-md-6">
+                        <button type="submit" class="btn btn-danger ftco-animate">Tambah Cabang
+                        <span></span>
+                        <button type="reset" onclick="history.back()" class="btn btn-default ftco-animate">Batal
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
             </div>
         </div>
+      </div>
     </div>
+  </div>
+</div>
+<br/>
 @endsection
+@section('script')
+<script src="{{ asset('js/parsleyjs/dist/parsley.min.js') }}"></script>
+<script type="text/javascript">
+    function init_parsley() {
+
+      if (typeof(parsley) === 'undefined') {
+        return;
+      }
+      console.log('init_parsley');
+
+      $ /*.listen*/ ('parsley:field:validate', function() {
+        validateFront();
+      });
+
+      $('#form-valid .btn').on('click', function() {
+        $('#form-valid').parsley().validate();
+        validateFront();
+      });
+      var validateFront = function() {
+        if (true === $('#form-valid').parsley().isValid()) {
+          $('.bs-callout-info').removeClass('hidden');
+          $('.bs-callout-warning').addClass('hidden');
+        } else {
+          $('.bs-callout-info').addClass('hidden');
+          $('.bs-callout-warning').removeClass('hidden');
+        }
+      };
+
+      try {
+        hljs.initHighlightingOnLoad();
+      } catch (err) {}
+
+    };
+
+    $(document).ready(function() {
+      init_parsley();
+
+    });
+  </script>
+  @endsection
--- resources/views/admin/cabang/index.blade.php
+++ resources/views/admin/cabang/index.blade.php
@@ -1,41 +1,134 @@
 @extends('layouts.app')
-
+@section('title', 'Daftar STO')
+@section('style')
+<link href="{{asset('css/datatables/dataTables.bootstrap.min.css')}}" rel="stylesheet">
+<link href="{{asset('css/datatables/responsive.bootstrap.min.css')}}" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="{{asset('css/sweetalert/sweetalert.min.css')}}">
+@endsection
 @section('content')
-    <div class="col-lg-9">
-        <div class="panel panel-default">
-            <div class="panel-heading">List STO</div>
-
-            <div class="panel-body">
-                @if (session('status'))
-                    <div class="alert alert-success">
-                        {{ session('status') }}
-                    </div>
-                @endif
-                <div>
-                    <a href="{{route('admin.model.form', ['model' => 'sto'])}}" class="btn btn-sm btn-primary">Tambah STO</a>
-                </div>
-                <div class="table-responsive">
-                    <table class="table table-striped table-hover">
-                        <thead>
-                            <tr>
-                                <th>Kode STO</th>
-                                <th>Nama STO</th>
-                                <th>Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($allCabang as $sto)
-                            <tr>
-                                <td>{{$sto->kode}}</td>
-                                <td>{{$sto->nama}}</td>
-                                <td><a href="{{route('admin.model.updateForm', ['model' => 'sto', 'id' => $sto->id])}}" class="btn btn-xs btn-info">Sunting</a>
-                                <a href="{{route('admin.model.delete', ['model' => 'sto', 'id' => $sto->id])}}" class="btn btn-xs btn-danger">Hapus</a></td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                </div>
+<div class="right_col" role="main">
+  <div class="">
+    <div class="row">
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel">
+          <div class="row x_title">
+            <div class="col-md-6">
+              <h3>Daftar Cabang STO</h3>
             </div>
+            <div class="col-md-6">
+              <div>
+                <a href="{{route('admin.model.form', ['model' => 'sto'])}}" class="btn btn-default ftco-animate pull-right fa fa-plus-square" style="padding: 14px 20px; font-size: 15px;"> &nbsp &nbsp Tambah STO </a>
+              </div>
+            </div>
+          </div>
+            <div class="container">
+              <div class="x_content">
+                <table id="datatable-responsive" class="table table-striped table-vcenter" cellspacing="0" width="100%" alignright>
+                  <thead>
+                    <tr>
+                      <th class="text-center">Kode STO</th>
+                      <th class="text-center">Nama STO</th>
+                      <th class="text-center" style="width:20%">Tindakan</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @foreach($allCabang as $sto)
+                    <tr>
+                      <td>{{$sto->kode}}</td>
+                      <td>{{$sto->nama}}</td>
+                      <td>
+                        <center>
+                          <a class="btn btn-dark ftco-animate btn-sm" href="{{route('admin.model.updateForm', ['model' => 'sto', 'id' => $sto->id])}}">Ubah</a>
+                          <a onclick="hapusFunction('{{$sto->nama}}', '{{route('admin.model.delete', ['model' => 'sto', 'id' => $sto->id])}}')" class="btn btn-danger ftco-animate btn-sm">Hapus</a>
+                        </center>
+                      </td>
+                    </tr>
+                    @endforeach
+                  </tbody>
+                </table>
+                <form id="hapus" method="get" style="display:none;" action="{{route('admin.model.delete', ['model' => 'sto', 'id' => $sto->id])}}"></form>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
+<br />
+@endsection
+@section('script')
+<script src="{{ asset('js/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('js/datatables/dataTables.bootstrap.min.js') }}"></script>
+<script src="{{asset('js/sweetalert/sweetalert.min.js')}}"></script>
+<script type="text/javascript">
+    //Table
+    function init_DataTables() {
+
+      console.log('run_datatables');
+
+      if (typeof($.fn.DataTable) === 'undefined') {
+        return;
+      }
+      console.log('init_DataTables');
+
+      $('#datatable').dataTable();
+
+      $('#datatable-keytable').DataTable({
+        keys: true
+      });
+
+      $('#datatable-responsive').DataTable();
+
+      $('#datatable-scroller').DataTable({
+        ajax: "js/datatables/json/scroller-demo.json",
+        deferRender: true,
+        scrollY: 380,
+        scrollCollapse: true,
+        scroller: true
+      });
+
+      $('#datatable-fixed-header').DataTable({
+        fixedHeader: true
+      });
+
+    };
+
+    // swal Hapus
+
+    function hapusFunction(namacabang, actionPath) {
+      console.log(actionPath);
+      event.preventDefault(); // prevent form submit
+      swal({
+          title: "Hapus " + namacabang + "?",
+          text: "Setelah dihapus, STO " +namacabang+ " tidak akan terintegrasi lagi di I-CX.",
+          type: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#DD6B55",
+          confirmButtonText: "Hapus",
+          cancelButtonText: "Batal",
+          closeOnConfirm: false,
+          closeOnCancel: false
+        },
+        function(isConfirm) {
+          if (isConfirm) {
+            var hapusForm = document.getElementById("hapus");
+            hapusForm.action = actionPath;
+            hapusForm.submit();
+          } else {
+            swal("Batal", "STO " +namacabang + " tidak terhapus.",);
+          }
+        });
+    }
+
+    $(document).ready(function() {
+      init_DataTables()
+    });
+  </script>
+
+  @if (session('status'))
+  <script>
+      swal("Berhasil", "{{ session('status') }}", "success");
+  </script>
+  @endif
+
 @endsection
